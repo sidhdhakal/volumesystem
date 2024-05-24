98599e8d2e64dd394cc2f93401c1be8119505954
siddharthadhakal <siddharthadhakal54@gmail.com>
2024-05-24 12:39:15 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [volume, setVolume] = useState(50);
+
+  const increaseVol = () => {
+    if (volume < 100) {
+      setTimeout(() => {
+        setVolume(volume+ 10);
+      }, 9000);
+    }
+  };
+
+  const decreaseVol = () => {
+    if (volume > 0) {
+      setVolume(volume - 10);
+    }
+  };
+
+  return (
+    <>
+      <h5>{volume}</h5>
+      <button onClick={increaseVol}>+</button>
+      <button onClick={decreaseVol}>-</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>You can reach us using the following methods:</p>
+      <ul>
+        <li>Email: info@example.com</li>
+        <li>Phone: +1234567890</li>
+        <li>Address: 123 Street, City, Country</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ContactPage;
